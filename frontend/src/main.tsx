c7166de91361d101fa273a318ed7c8c72edfe6b3
Serkan K <serkan.kibritoglu@dbca.wa.gov.au>
2025-05-14 11:07:52 +0800

    Initialise frontend Vite + React

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import { router } from './router'
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+  
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+)
---
+++ frontend/src/router.tsx
@@ -0,0 +1,110 @@
+// router.tsx with error handling
+import { createBrowserRouter, redirect } from "react-router";
+import App from "./App";
+
+// Layouts
+// import AuthLayout from "@/components/layout/AuthLayout";
+// import MainLayout from "@/components/layout/MainLayout";
+
+// Pages
+// import Login from "@/routes/auth/Login";
+// import Register from "@/routes/auth/Register";
+// import Home from "@/routes/dashboard/Home";
+// import Users from "@/routes/users/Users";
+// import UserDetail from "@/routes/users/UserDetail";
+// import Submissions from "@/routes/submissions/Submissions";
+// import SubmissionDetail from "@/routes/submissions/SubmissionDetail";
+// import AdminPage from "./routes/admin/AdminPage";
+
+// Store utils
+// import { getAuthStore } from "./stores/storeUtils";
+
+// Error component
+// import ErrorPage from "./components/ErrorPage";
+
+// Guards
+// const authGuard = () => {
+// 	const authStore = getAuthStore();
+// 	if (!authStore.isAuthenticated) {
+// 		return redirect("/auth/login");
+// 	}
+// 	return null;
+// };
+
+// const guestGuard = () => {
+// 	const authStore = getAuthStore();
+// 	if (authStore.isAuthenticated) {
+// 		return redirect("/");
+// 	}
+// 	return null;
+// };
+
+// const adminGuard = () => {
+// 	const authStore = getAuthStore();
+// 	// default auth guard
+// 	if (!authStore.isAuthenticated) {
+// 		return redirect("/auth/login");
+// 	}
+// 	// Prevent access for non-admins by redirecting to dashboard
+// 	if (!authStore.isAdmin) {
+// 		return redirect("/");
+// 	}
+// 	return null;
+// };
+
+export const router = createBrowserRouter([
+	{
+		path: "/",
+		Component: App,
+	},
+	{
+		path: "/aec",
+		element: <div>Hello World</div>,
+	},
+	// {
+	// 	path: "/",
+	// 	// errorElement: <ErrorPage />,
+	// 	children: [
+	// 		// Auth routes (for non-authenticated users)
+	// 		{
+	// 			path: "auth",
+	// 			element: <AuthLayout />,
+	// 			loader: guestGuard,
+	// 			errorElement: <ErrorPage />,
+	// 			children: [
+	// 				{ path: "login", element: <Login /> },
+	// 				{ path: "register", element: <Register /> },
+	// 			],
+	// 		},
+
+	// 		// Protected routes (for authenticated users)
+	// 		{
+	// 			element: <MainLayout />,
+	// 			loader: authGuard,
+	// 			errorElement: <ErrorPage />,
+	// 			children: [
+	// 				{ index: true, element: <Home /> },
+	// 				{
+	// 					path: "users",
+	// 					children: [
+	// 						{ index: true, element: <Users /> },
+	// 						{ path: ":id", element: <UserDetail /> },
+	// 					],
+	// 				},
+	// 				{
+	// 					path: "submissions",
+	// 					children: [
+	// 						{ index: true, element: <Submissions /> },
+	// 						{ path: ":id", element: <SubmissionDetail /> },
+	// 					],
+	// 				},
+	// 				{
+	// 					path: "admin",
+	// 					element: <AdminPage />,
+	// 					loader: adminGuard,
+	// 				},
+	// 			],
+	// 		},
+	// 	],
+	// },
+]);
