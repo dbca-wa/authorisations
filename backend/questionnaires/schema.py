c2b19dff7236878fbff9729a0b445b09c1683d3d
dbca-serkan <serkan.kibritoglu@dbca.wa.gov.au>
2026-09-07 11:07:36 +0800

    Minor comment cleanup

--- backend/questionnaires/schema.py
+++ backend/questionnaires/schema.py
@@ -6,12 +6,8 @@ from jsonschema.validators import Draft202012Validator
 
 from .serialisers import QuestionSerialiser, SectionSerialiser, StepSerialiser
 
-# Current version of the schema (unsigned integer: 0, 1, 2, 3, ...)
-# Version 0: legacy baseline (2025.07-1)
-# Version 1: Consolidate question config (2025.08-1) - moves select_options, grid_columns, 
-#            grid_max_rows, dependent_step, file_max_attachments into nested config object
-# Version 2+: future ordinal migrations
-# Previous versions are maintained in schema_migrations/ directory
+# Current schema version (unsigned integer: 0, 1, 2, ...)
+# Migrations stored in schema_migrations/ directory:
 SCHEMA_VERSION = 1
 
 # This should never be modified on runtime, therefore we use frozendict
